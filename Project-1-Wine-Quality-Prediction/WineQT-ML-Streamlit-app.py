a976cb80309c3eca0c02cfa98d1e0037bdbc2048
Adrian Dominguez-Castro <157868503+DrAdrianDC@users.noreply.github.com>
2024-04-15 03:55:17 -0700

    Update WineQT-ML-Streamlit-app.py

--- Project-1-Wine-Quality-Prediction/WineQT-ML-Streamlit-app.py
+++ Project-1-Wine-Quality-Prediction/WineQT-ML-Streamlit-app.py
@@ -388,28 +388,39 @@ elif state == "wine_quality_prediction":
       st.write("<br>", unsafe_allow_html=True)
       st.write("")
       
-# Definir la función para predecir la calidad del vino
       def predict_quality(fixed_acidity, volatile_acidity, citric_acidity, residual_sugar, chlorides,
-                     free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol):
-       input_data = np.array([[fixed_acidity, volatile_acidity, citric_acidity, residual_sugar, chlorides,
-                             free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]])
-       prediction = model.predict(input_data)
+                    free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol):
+    # Check if all input values are provided
+          if None in [fixed_acidity, volatile_acidity, citric_acidity, residual_sugar, chlorides,
+                free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]:
+             return "Please provide all input values."
+
+    # Convert input values to float
+      input_data = np.array([[float(fixed_acidity), float(volatile_acidity), float(citric_acidity), float(residual_sugar),
+                             float(chlorides), float(free_sulfur_dioxide), float(total_sulfur_dioxide),
+                             float(density), float(pH), float(sulphates), float(alcohol)]])
+
+    # Check if model is loaded
+      if 'model' not in globals():
+         return "Model not loaded. Please load the model first."
+
+    # Make prediction
+      prediction = model.predict(input_data)
        return prediction
-    
-      if st.button("Predict", key="predict_button"):
-        prediction = predict_quality(fixed_acidity, volatile_acidity, citric_acidity, residual_sugar, chlorides,
-                                  free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol)
-        
-        
-        # Mostrar el resultado de la predicción
+
+     if st.button("Predict", key="predict_button"):
+       prediction = predict_quality(fixed_acidity, volatile_acidity, citric_acidity, residual_sugar, chlorides,
+                                 free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol)
+
+    # Display the prediction result
+     if isinstance(prediction, str):
+        st.write(prediction)
+     else:
         if prediction[0] == 0:
             st.write("Predicted wine quality: Bad")
         elif prediction[0] == 1:
             st.write("Predicted wine quality: Good")
-        
-      #  st.write(f"Predicted wine quality: {prediction[0]}")
-        st.write("")
-        st.markdown("")
+
         
 
          
