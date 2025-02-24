bd9a152aaf0217ef7469a72f2be8e0cb073ffe4e
Adrian Dominguez-Castro <157868503+DrAdrianDC@users.noreply.github.com>
2025-02-24 13:13:09 -0800

    Update WineQT-ML-Streamlit-app.py

--- Project-2-Wine-Quality-Prediction/WineQT-ML-Streamlit-app.py
+++ Project-2-Wine-Quality-Prediction/WineQT-ML-Streamlit-app.py
@@ -348,7 +348,7 @@ elif state == "wine_quality_prediction":
 
      # URL of the model on GitHub
      # model_url = 'https://github.com/DrAdrianDC/Portfolio-for-Data-Science/raw/main/Project-2-Wine-Quality-Prediction/XGBoost/XGBoost_joblib.pkl'
-      model_url = 'https://github.com/DrAdrianDC/Portfolio-for-Data-Science/raw/main/Project-2-Wine-Quality-Prediction/XGBoost_joblib.pkl'
+      model_url = 'https://github.com/DrAdrianDC/Portfolio-for-Data-Science/blob/main/Project-2-Wine-Quality-Prediction/XGBoost_joblib.pkl'
     # Download the model file
       response = requests.get(model_url)
 
